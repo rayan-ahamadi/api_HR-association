16815b6abede023e8fefaa871e26a5811a3e84b0
rayan-ahamadi <115161695+rayan-ahamadi@users.noreply.github.com>
2023-10-31 03:03:42 +0100

    Partie 3 du cours openclassrooms, création d'une API REST avec H2 en mémoire

---
+++ api/src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.openclassrooms.api.controller;
+
+import com.openclassrooms.api.model.Employee;
+import com.openclassrooms.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+}
